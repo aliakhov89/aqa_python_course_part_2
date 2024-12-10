0d1e54622239f10021fa89fe3b2bec02c7d65f42
aliakhov89 <andrii.liakhov.0201@gmail.com>
2024-12-10 15:23:44 +0200

    Tesks from part 1_1

---
+++ part_1_1/task_1.py
@@ -0,0 +1,7 @@
+#Чтобы мы могли убедиться в том, что вы действительно установили интерпретатор, исполните построчно в интерпретаторе Python следующий код:
+
+#fib = lambda x : 1 if x <= 2 else fib(x - 1) + fib(x - 2)
+#fib(31)
+
+fib = lambda x : 1 if x <= 2 else fib(x - 1) + fib(x - 2)
+print(fib(31)) #added print() by myself - because I didn't see result after running code
